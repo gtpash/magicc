f5bab2b16ffb4acdefef467712f9e08c75b56230
Graham Pash <gtpash96@gmail.com>
2022-04-18 13:21:17 -0500

    update with sv decay functions

--- rxdif_with_treatment.m
+++ rxdif_with_treatment.m
@@ -1,3 +1,68 @@
+%{ 
+rxdif_with_treatment: creates virtual tumors with known parameters for building POD operators
+Formatting:
+    - Struct: named after kp and d values, kp0_000d0_000, decimal replaced with underscore
+        - .kp: proliferation used for fwd eval
+        - .d: diffusivity used for fwd eval
+        - .N_0: initial seeding density
+        - .N: Snapshots
+        - .t: times of snapshots
+    Repeated for each combination of kp and d to be tested
+%}
+
+clear; clc; close all;
+
+% parameter bounds
+kp_vec = [0.1, 0.2];
+d_vec = [0.1, 0.5, 1.0];
+
+n = 50;
+
+% initializations
+N0 = initialize_tumor(n);
+params.dt = 0.1;    % time step [day]
+params.h = 1;       % spatial discretization [mm]
+params.tspan = [0, 56];
+params.k = kp_vec(2);
+params.d = d_vec(3);
+
+[N, t] = FTCS_tx(N0, params);
+
+cnrg = plotsv(N);
+CNRG_THRESHOLD = 0.99;
+n_cnrg = numel(cnrg(cnrg < CNRG_THRESHOLD));
+fprintf("Modes required for Cumulative Energy to be %.3f:\t%i\n", CNRG_THRESHOLD, n_cnrg);
+
+% loop through (d, kp) combos
+% for jj = 1:length(d_vec)
+%     for ii = 1:length(kp_vec)
+%         kp = kp_vec(ii);
+%         d = d_vec(jj);
+% 
+%         kp_str = replace(num2str(kp,'%.3f'),'.','_');
+%         d_str  = replace(num2str(d,'%.3f'),'.','_');
+%         name = ['kp',kp_str,'d',d_str];
+% 
+%         
+% 
+%     end
+% end
+
+function [N0] = initialize_tumor(n)
+%Initialize 50x50 grid
+x = 1:1:n;
+y = 1:1:n;
+[X,Y] = meshgrid(x,y);
+x_mid = (n+1)/2; y_mid = x_mid; %Center Point
+
+z = exp(-((X-x_mid).^2+(Y-y_mid).^2)/5);
+
+%Initialize tumor seed with 50% cell density
+N0 = zeros(size(z));
+N0(z>0.05) = 0.5;
+N0 = N0(:);
+end
+
 function [L] = assembleL(n)
 I = eye(n);
 e = ones(n,1);
@@ -35,7 +100,7 @@ end
 
 
 function [H] = assembleH(n, k)
-H = zeros(n, n^2);
+H = sparse(zeros(n, n^2));
 H(:, 1:n+1:end) = eye(n);
 H = k*sparse(H);
 end
@@ -43,6 +108,11 @@ end
 
 function [N] = treatment(N, C, alpha1, alpha2, beta1, beta2, t)
     (alpha1*exp(-beta1*t) + alpha2*exp(-beta2*t))*C*N;
+end
+
+
+function [N] = proliferation(N, k, theta)
+N = k*N.*(1 - N/theta);
 end
 
 
@@ -59,31 +129,65 @@ function [N, t] = FTCS_tx(N0, params)
 % unpack parameters
 tspan = params.tspan;
 dt = params.dt;
-txduration = params.txduration;
-alpha1 = params.alpha1;
-alpha2 = params.alpha2;
-beta1 = params.beta1;
-beta2 = params.beta2;
+h = params.h;
+k = params.k;
+d = params.d;
+theta = 1;
+if isfield(params, 'treatment')
+    treatment = params.treatment;
+else
+    treatment = false;
+end
+if treatment
+    txduration = params.txduration;
+    alpha1 = params.alpha1;
+    alpha2 = params.alpha2;
+    beta1 = params.beta1;
+    beta2 = params.beta2;
+    nt_tx = numel(0:dt:txduration);
+end
 
 % simulation times
 t = tspan(1):dt:tspan(2);
 nt = numel(t);
-nt_tx = numel(0:dt:txduration);
 
 % matrix to store output
 N = zeros(numel(N0), nt);
 N(:,1) = N0;
 
 % form operators
-n = sqrt(numel(N0));
-L = assembleL(n);
+L = assembleL(sqrt(numel(N0)));
 L = applyBC(L);
-H = assembleH(n, k);
+% H = assembleH(n, k);
 
 % forward euler time-stepping
 for i = 1:nt-1
-    N(:, i+1) = N(:, i) + dt*(d/h^2)*L*N(:, i) + dt*k*eye(n)*N(:, i) ...
-        - dt*H*kron(N(:, i), N(:, i)) ...
-        - dt*treatment(N, C, alpha1, alpha2, beta1, beta2, mod(nt, nt_tx));
+    if treatment
+        treat = dt*treatment(N, C, alpha1, alpha2, beta1, beta2, mod(nt, nt_tx));
+    else
+        treat = 0;
+    end
+
+    N(:, i+1) = N(:, i) + dt*(d/h^2)*L*N(:, i) + proliferation(N(:, i), k, theta) - treat;
+%     N(:, i+1) = N(:, i) + dt*(d/h^2)*L*N(:, i) + dt*k*eye(n)*N(:, i) ...
+%         - dt*H*kron(N(:, i), N(:, i)) - treat;    
+
 end
+end
+
+function [cnrg] = plotsv(N)
+    sv = svd(N);
+    figure; 
+    semilogy(sv/max(sv), 'k', 'linewidth', 3);
+    title('Singular Value Decay of Snapshot Matrix');
+    xlabel('i');
+    ylabel('$\sigma_i/\|\sigma\|_\infty$', 'interpreter', 'latex');
+
+    figure;
+    cnrg = cumsum(sv.^2);
+    cnrg = cnrg/cnrg(end);
+    plot(cnrg, 'kx', 'LineWidth', 3);
+    ylabel('Cumulative Energy', 'interpreter', 'latex', 'FontSize', 16);
+    ylim([0, 1]);
+    xlabel('$i$', 'interpreter', 'latex', 'FontSize', 16);
 end
